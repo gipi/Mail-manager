f3edddceec86c39e6d97e4d763921c64611422e5
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-11-10 20:00:37 +0100

    Add conf command.

---
+++ mailmanager/commands/conf.py
@@ -0,0 +1,42 @@
+from .base import BaseCommand
+import sys
+import tarfile
+import StringIO
+
+
+class Command(BaseCommand):
+    """Print out a tar archive containing all the configuration file
+    needed for your system.
+    """
+    def build_tarinfo(self, filepath, content):
+        info = tarfile.TarInfo(filepath)
+        fobject = StringIO.StringIO(content)
+        info.size = len(fobject.buf)
+
+        return {'tarinfo': info, 'fileobj': fobject}
+
+    def handle(self, *args):
+        if sys.stdout.isatty():
+            self.error('a tty has been detected, probably you want to pipe this to \'tar xf - -C /your/path/\'')
+
+        params = self.get_configuration_params()
+
+        output = StringIO.StringIO()
+        with tarfile.open(mode='w:', fileobj=output) as tar:
+            tar.addfile(**self.build_tarinfo(
+                '/etc/dovecot/dovecot.conf',
+                self.render_template('dovecot.conf', params)))
+            tar.addfile(**self.build_tarinfo(
+                '/etc/dovecot/dovecot-sql.conf',
+                self.render_template('dovecot-sql.conf', params)))
+            tar.addfile(**self.build_tarinfo(
+                '/etc/postfix/main.cf',
+                self.render_template('main.cf', params)))
+            tar.addfile(**self.build_tarinfo(
+                '/etc/postfix/mailbox_domains.cf',
+                self.render_template('mailbox_domains.cf', params)))
+            tar.addfile(**self.build_tarinfo(
+                '/etc/postfix/mailbox_maps.cf',
+                self.render_template('mailbox_maps.cf', params)))
+
+        print output.getvalue()
